59a03fa0153a22f79f98083f3f79ba30d164ee57
Nikola <nikola.cvetkovic91@gmail.com>
2023-10-10 13:39:25 +0200

    add missing upc attribute to BeerOrderLine

--- src/main/java/xyz/riocode/beer/order/service/domain/BaseEntity.java
+++ src/main/java/xyz/riocode/beer/order/service/domain/BaseEntity.java
@@ -47,6 +47,7 @@ public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
     @Version
