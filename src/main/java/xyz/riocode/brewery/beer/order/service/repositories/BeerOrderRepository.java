3234d2ff260ed6286bc3874e7e407a4c2acca284
Nikola <nikola.cvetkovic91@gmail.com>
2023-10-26 16:02:31 +0200

    cleanup

--- src/main/java/xyz/riocode/brewery/beer/order/service/repositories/BeerOrderRepository.java
+++ src/main/java/xyz/riocode/brewery/beer/order/service/repositories/BeerOrderRepository.java
@@ -29,10 +29,6 @@ import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.UUID;
 
-
-/**
- * Created by jt on 2019-01-26.
- */
 public interface BeerOrderRepository  extends JpaRepository<BeerOrder, UUID> {
 
     Page<BeerOrder> findAllByCustomer(Customer customer, Pageable pageable);
