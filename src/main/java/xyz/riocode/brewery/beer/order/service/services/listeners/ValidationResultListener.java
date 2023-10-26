31351e832cd730e3193f496807951c940f1a34e2
Nikola <nikola.cvetkovic91@gmail.com>
2023-10-27 01:02:14 +0200

    fix event name

--- src/main/java/xyz/riocode/brewery/beer/order/service/services/listeners/ValidationResultListener.java
+++ src/main/java/xyz/riocode/brewery/beer/order/service/services/listeners/ValidationResultListener.java
@@ -5,7 +5,7 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import xyz.riocode.brewery.beer.order.service.config.JmsConfig;
 import xyz.riocode.brewery.beer.order.service.services.BeerOrderManager;
-import xyz.riocode.brewery.common.events.ValidateOrderResultEvent;
+import xyz.riocode.brewery.common.events.ValidateBeerOrderResultEvent;
 
 @RequiredArgsConstructor
 @Component
@@ -14,7 +14,7 @@ public class ValidationResultListener {
     private final BeerOrderManager beerOrderManager;
 
     @JmsListener(destination = JmsConfig.VALIDATE_BEER_ORDER_RES_QUEUE)
-    public void listen(ValidateOrderResultEvent event) {
+    public void listen(ValidateBeerOrderResultEvent event) {
         beerOrderManager.processValidationResult(event.getOrderId(), event.getIsValid());
     }
 
