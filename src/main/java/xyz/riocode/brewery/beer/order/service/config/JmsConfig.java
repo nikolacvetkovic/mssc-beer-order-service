8f2f116c341fde67819b11978ba2068f477efd45
Nikola <nikola.cvetkovic91@gmail.com>
2023-10-30 13:03:10 +0100

    add missing relationship between message converter and jacksong object mapper

--- src/main/java/xyz/riocode/brewery/beer/order/service/config/JmsConfig.java
+++ src/main/java/xyz/riocode/brewery/beer/order/service/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package xyz.riocode.brewery.beer.order.service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -14,10 +15,11 @@ public class JmsConfig {
     public static final String ALLOCATE_BEER_ORDER_REQ_QUEUE = "allocate-beer-order-request";
     public static final String ALLOCATE_BEER_ORDER_RES_QUEUE = "allocate-beer-order-response";
     @Bean
-    public MessageConverter messageConverter() {
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
